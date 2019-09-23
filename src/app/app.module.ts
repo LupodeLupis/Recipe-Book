e43747064988fc580f90e31f643c499410d44a47
Alberto Laurenzi <35709073+LupodeLupis@users.noreply.github.com>
2019-09-22 20:50:39 -0400

    added componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './modules/recipe-book/components/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './modules/recipe-book/components/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './modules/recipe-book/components/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './modules/shopping-list/components/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule

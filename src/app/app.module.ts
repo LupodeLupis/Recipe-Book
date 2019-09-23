17ea3b839a0933c43659aead3d83b7865135cb8c
Alberto Laurenzi <35709073+LupodeLupis@users.noreply.github.com>
2019-09-22 21:14:53 -0400

    setting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RecipeListComponent } from './modules/recipe-book/components/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './modules/recipe-book/components/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './modules/recipe-book/components/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './modules/recipe-book/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './modules/recipe-book/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './modules/recipe-book/recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './modules/shopping-list/components/shopping-list/shopping-list.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingEditComponent } from './modules/shopping-list/components/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesComponent } from './modules/recipe-book/recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { ShoppingEditComponent } from './modules/shopping-list/components/shoppi
     ShoppingListComponent,
     HeaderComponent,
     ShoppingEditComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule

0fc546763289075572a320f9008c9950aec10cde
Alberto Laurenzi <35709073+LupodeLupis@users.noreply.github.com>
2019-09-22 21:02:10 -0400

    setting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './modules/recipe-book/components/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './modules/recipe-book/components/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './modules/recipe-book/components/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './modules/recipe-book/components/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './modules/shopping-list/components/shopping-list/shopping-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ShoppingListComponent } from './modules/shopping-list/components/shoppi
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule

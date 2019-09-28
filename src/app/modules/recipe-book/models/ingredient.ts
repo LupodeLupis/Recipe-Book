7ad445e42a23fb3bb4ae647e1a72f18fbe10622a
Alberto Laurenzi <35709073+LupodeLupis@users.noreply.github.com>
2019-09-28 12:20:35 -0400

    working on recipe list

---
+++ src/app/modules/recipe-book/models/ingredient.ts
@@ -0,0 +1,18 @@
+export interface Recipe {
+    name: string;
+    description:string;
+    imgeURL: string;
+}
+
+/*export class Recipe {
+    name: string;
+    description:string;
+    imgeURL: string;
+
+    constructor(name:string, desc:string, imgUrl:string){
+        this.name = name;
+        this.description = desc;
+        this.imgeURL = imgUrl;
+        
+    }
+}*/
--- src/app/modules/recipe-book/recipes/recipe-list/recipe-list.component.ts
+++ src/app/modules/recipe-book/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../models/ingredient';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+  recipes: Recipe[];
+  // recipes: Recipe[] = [new Recipe ("Test recipe","Test recipe description" , "https://pixabay.com/photos/food-meat-recipe-power-pork-1459693/")] /*this line use classes as model but is not suggested */
+  constructor() {
+    this.recipes = [];
+   }
 
   ngOnInit() {
+    console.log("ngOnInit() is called")
+    this.recipes.push(
+      {
+        name: "Test recipe",
+        description:"Test recipe description",
+        imgeURL:"https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      },
+      {
+        name: "Test recipe2",
+        description:"Test recipe description2",
+        imgeURL:"https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      }
+    )
   }
 
 }

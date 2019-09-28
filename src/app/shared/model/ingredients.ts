95e63141bdd5e8f102a2832ec74312e43c2f3bee
Alberto Laurenzi <35709073+LupodeLupis@users.noreply.github.com>
2019-09-28 15:26:38 -0400

    working on template component

---
+++ src/app/shared/model/ingredients.ts
@@ -0,0 +1,20 @@
+
+
+export interface Ingredient {
+    name:string;
+    amount:number
+}
+
+/*export class Ingredient {
+    name: string;
+    amount: number;
+    constructor(name:string, amount:number){
+        this.name = name;
+        this.amount = amount;
+    }
+}
+/// The follows class  works in the same way as the above class
+export class Ingredient {
+    constructor(public name: string, public amount:number) {}
+}*/
+
